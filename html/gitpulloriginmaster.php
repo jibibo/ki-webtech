5a53ed1ed4c7a9a57108e697396d9c7e49b5127a
Julian <julianibibo@gmail.com>
2022-01-18 17:22:22 +0100

    execpint add

--- html/gitpulloriginmaster.php
+++ html/gitpulloriginmaster.php
@@ -1,10 +1,26 @@
 <?php
 
-$output = shell_exec("git pull origin master");
+// src: https://stackoverflow.com/questions/23136044/automation-of-git-pull-using-php-code
+function execPrint($command) {
+  $result = array();
+  exec($command, $result);
+  print("<pre>");
+  foreach ($result as $line) {
+      print($line . "\n");
+  }
+  print("</pre>");
+}
 
-echo $output;
+// Print the exec output inside of a pre element
+execPrint("git pull origin master");
+execPrint("git status");
 
-echo "test pull 123123";
-echo "jull";[[]]
+
+// $output = shell_exec("git pull origin master");
+
+// echo $output;
+
+// echo "test pull 123123";
+// echo "jull";
 
 ?>
