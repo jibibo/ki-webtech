40ca373dafae99ad9115d155f8960e41deb3631b
Xinyu <xinyu.zhu2@student.uva.nl>
2022-01-19 00:32:48 +0100

    hi

--- html/login.php
+++ html/login.php
@@ -71,10 +71,10 @@
                 </div>
             </form>
         </div>
-        <?php
-        include "footer.php";
-        ?>
-        
+      <?php
+      include "footer.php";
+      ?>
+
       <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
       <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
     
