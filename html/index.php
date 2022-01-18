50d42473cfb0f420efe13cc886ebe93032f85611
Xinyu <xinyu.zhu2@student.uva.nl>
2022-01-18 23:40:49 +0100

    back

--- html/index.php
+++ html/index.php
@@ -38,8 +38,8 @@
             <form action="">
               <div class="textbar">
                 <input type="text" placeholder="  Search for..." name="search">
-              </div>
-              <div class="buttonbox">
+                <!--</div>
+                <div> -->
                 <button type="submit" class="searchbutton">
                   <span class="icon">
                     <ion-icon name="search-outline"></ion-icon>
