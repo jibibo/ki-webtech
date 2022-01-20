461ef84d9686b8e65b94c260a21404edc6a40e02
Xinyu <xinyu.zhu2@student.uva.nl>
2022-01-20 23:05:55 +0100

    css forgot password

--- html/forgot-password.php
+++ html/forgot-password.php
@@ -10,7 +10,6 @@
   <link rel="stylesheet" href="css/global.css" />
   <link rel="stylesheet" href="css/navbar.css" />
   <link rel="stylesheet" href="css/footer.css" />
-  <link rel="stylesheet" href="css/login.css" />
   <link rel="stylesheet" href="css/forgot-password.css" />
 
   <style>
