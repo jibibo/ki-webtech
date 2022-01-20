ecdd90110f243026901721922177fa6f7b688715
Julian <julianibibo@gmail.com>
2022-01-20 20:34:47 +0100

    products css

--- html/product.php
+++ html/product.php
@@ -96,11 +96,21 @@ include "db_disconnect.php";
 
 <head>
   <title><?php echo $product_name ?> | UvAzon</title>
-  <link rel="stylesheet" href="css/product.css" />
+  <meta charset="utf-8" />
+  <meta http-equiv="x-ua-compatible" content="ie=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <link rel="icon" type="image/x-icon" href="/images/favicon.ico" />
   <link rel="stylesheet" href="css/global.css" />
+  <link rel="stylesheet" href="css/navbar.css" />
+  <link rel="stylesheet" href="css/footer.css" />
+  <link rel="stylesheet" href="css/product.css" />
 </head>
 
 <body>
+  <?php
+  include "navbar.php";
+  ?>
+
   <div class="container">
     <div class="product-details">
       <div class="product-left padding">
@@ -225,6 +235,10 @@ include "db_disconnect.php";
       </div>
     </div>
   </div>
+
+  <?php
+  include "footer.php";
+  ?>
 </body>
 
 </html>
--- html/products.php
+++ html/products.php
@@ -36,16 +36,19 @@ include "db_disconnect.php";
 
       if (mysqli_num_rows($result) > 0) {
         while ($product = mysqli_fetch_assoc($result)) {
+          $product_id = $product["id"];
           $product_image_url = $product["image_url"];
           $product_name = $product["name"];
           $product_price = number_format($product["price"], 2);
           echo "
           <div class='product'>
-            <img class='product-image' src='$product_image_url'/>
+            <a href='product.php?id=$product_id'>
+              <img class='product-image' src='$product_image_url'/>
+            </a>
             <span class='product-name'>$product_name</span>
             <div class='product-details'>
               <span class='product-price'>&euro; $product_price</span>
-              <button class='product-wishlist'>+ wishlist</button>
+              <button class='product-buy'>Buy!</button>
             </div>
           </div>
           ";
@@ -56,25 +59,8 @@ include "db_disconnect.php";
 
       ?>
 
-      <div class="product">
-        <img src="https://www.automobielmanagement.nl/wp-content/uploads/2019/04/Lada-Combi-1600x1123.jpg" />
-        <span class="product-name">[product name]</span>
-        <div class="product-details">
-          <span class="product-price">&euro; [product price]]</span>
-          <button class="product-wishlist">+ wishlist</button>
-        </div>
-      </div>
+      
 
-      <div class="product">
-        <div class="product-left">
-          <img src="https://www.automobielmanagement.nl/wp-content/uploads/2019/04/Lada-Combi-1600x1123.jpg" />
-
-        </div>
-        <div class="product-right">
-          <span class="product-name">[product name]</span>
-
-        </div>
-      </div>
     </div>
   </div>
 
