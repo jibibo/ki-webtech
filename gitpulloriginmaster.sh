3e16a317629c742abf3eaa0af8135f23650f3d13
PhilipAhmadzada <p.klimenko@hotmail.com>
2022-01-20 23:31:06 +0100

    fixed body checkout page

--- gitpulloriginmaster.sh
+++
@@ -1,1 +0,0 @@
-cd /var/www && git pull origin master
---
+++ html/dbconnect.php
@@ -0,0 +1,9 @@
+<?php 
+
+$username = 'root';
+$password = '';
+$dbName = 'uvazon';
+
+$conn = new mysqli('localhost', $username, $password, $dbName) or die("Connection unsuccessfull");
+
+?>
