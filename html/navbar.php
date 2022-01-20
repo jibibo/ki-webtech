91845192b4c137e0009376a2733161bb3ebf31ec
Xinyu <xinyu.zhu2@student.uva.nl>
2022-01-21 00:05:36 +0100

    verandering teruggezet

--- html/navbar.php
+++ html/navbar.php
@@ -1,6 +1,6 @@
 <nav>
   <div class="logo">
-    <a href="index.php"><img src="images/uvazon.png" alt="UvAzon Logo" width="250px"/></a>
+    <img src="images/uvazon.png" alt="UvAzon Logo" width="250px" />
   </div>
   <ul id="nav-ul">
     <li><a href="index.php">Home</a></li>
