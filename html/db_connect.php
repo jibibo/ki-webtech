992d8428c3018e292797b438ec5cea46e8470c8a
PhilipAhmadzada <p.klimenko@hotmail.com>
2022-01-20 23:45:28 +0100

    test

--- html/db_connect.php
+++ html/db_connect.php
@@ -3,15 +3,12 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 'On');
 
-# include "db_credentials.php";
+include "db_credentials.php";
 
 $db_server = "localhost";
 $db_name = "uvazon";
-$db_username = "root";
-$db_password = "";
 
 $conn = mysqli_connect($db_server, $db_username, $db_password, $db_name);
-
 if (!$conn) {
   die("DB connection failed: " . mysqli_connect_error());
 }
