1555cca5c09b598c8e2027c43302c72a5beb8d0b
Xinyu <xinyu.zhu2@student.uva.nl>
2022-01-24 14:11:57 +0100

    sign up php

--- html/sign-up.php
+++ html/sign-up.php
@@ -2,7 +2,7 @@
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (empty($POST["email"])) {
         $email = "";
-    } elseif (!empty($POST["email"]) && !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    } elseif (!empty($POST["email"]) && filter_var($email, FILTER_VALIDATE_EMAIL)) {
         $email = htmlspecialchars($POST["email"]);
         /*if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
             $email = "";
