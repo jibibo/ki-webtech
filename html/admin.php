eea71e898750781abbbd493521adaa58ab7be003
Julian <julianibibo@gmail.com>
2022-01-30 18:12:01 +0100

    PRODUCTS

---
+++ html/admin.php
@@ -0,0 +1,78 @@
+<?php
+
+include "db_connect.php";
+
+if (!isset($_COOKIE["session_token"])) {
+  header("Location: index.php");
+  return;
+}
+
+$session_token = htmlspecialchars($_COOKIE["session_token"]);
+
+$result = mysqli_query($conn, "
+  SELECT * FROM 
+");
+
+include "db_disconnect.php";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <title>Admin | UvAzon</title>
+  <meta charset="utf-8" />
+  <meta http-equiv="x-ua-compatible" content="ie=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <link rel="icon" type="image/x-icon" href="/images/favicon.ico" />
+  <link rel="stylesheet" href="css/global.css" />
+  <link rel="stylesheet" href="css/navbar.css" />
+  <link rel="stylesheet" href="css/footer.css" />
+  <link rel="stylesheet" href="css/admin.css" />
+</head>
+
+<body>
+  <?php include "navbar.php" ?>
+
+  <div class="container">
+    <h1>
+      Admin page
+    </h1>
+
+    <h2>
+      Add product
+    </h2>
+
+    <form action="admin.php" method="POST">
+      <input type="text" name="name" placeholder="Name" autofocus />
+      <textarea name="description" placeholder="Description"></textarea>
+      <input type="number" name="price" step="0.01" placeholder="Price" />
+      <input type="text" name="image_url" placeholder="Image URL" />
+      <input type="submit" value="Add product" />
+      <input type="hidden" name="action" value="add_product" />
+    </form>
+
+    <h2>
+      Add category
+    </h2>
+
+    <!-- laat alleen "admin" op navbar zien als ingelogde user een admin is -->
+
+    <div class="categories-list">
+
+    </div>
+
+    <form action="admin.php" method="POST">
+      <input type="text" name="name" placeholder="name" autofocus />
+      <input type="submit" value="Add category" />
+      <input type="hidden" name="action" value="add_category" />
+    </form>
+  </div>
+
+
+
+  <?php include "footer.php" ?>
+</body>
+
+</html>
--- html/products.php
+++ html/products.php
@@ -1,9 +1,84 @@
 <?php
 
 include "db_connect.php";
+include "utils.php";
 
-$query = "SELECT * FROM products";
-$result = mysqli_query($conn, $query);
+// src: https://stackoverflow.com/a/6324360/13216113
+$result_categories = mysqli_query(
+  $conn,
+  "SELECT name, id, COUNT(*)
+  FROM categories c 
+    JOIN product_categories pc ON c.id=pc.category 
+  GROUP BY c.name 
+  ORDER BY COUNT(*) DESC, c.name;"
+);
+$categories = array();
+while ($row = mysqli_fetch_assoc($result_categories)) {
+  $categories[] = $row;
+}
+
+// src: https://stackoverflow.com/a/1222248/13216113
+$categories_checked = array();
+foreach ($_GET as $key => $values) {
+  if ($key != "c" || !is_array($values)) {
+    continue;
+  }
+
+  foreach ($values as $category_raw) {
+    // src: https://www.w3schools.com/php/func_var_is_numeric.asp
+    if (!is_numeric($category_raw)) {
+      continue;
+    }
+
+    $category_id = intval($category_raw);
+    if (!is_int($category_id)) {
+      continue;
+    }
+
+    $categories_checked[] = $category_id;
+  }
+}
+
+pre_print($categories_checked);
+
+if (isset($_GET["search"])) {
+  $categories_join = join(",", $categories_checked);
+  $search = htmlspecialchars($_GET["search"]);
+
+  if ($categories_join) {
+    if ($search) {
+      // src: https://stackoverflow.com/a/16082874/13216113
+      $products_query = "SELECT *
+        FROM products p 
+          JOIN product_categories pc ON p.id=pc.product 
+        WHERE pc.category IN ($categories_join)
+          AND LOWER(p.name) LIKE LOWER('%$search%');";
+    } else {
+      $products_query = "SELECT *
+      FROM products p 
+        JOIN product_categories pc ON p.id=pc.product 
+      WHERE pc.category IN ($categories_join);";
+    }
+  } else {
+    if ($search) {
+      $products_query = "SELECT *
+        FROM products p 
+          JOIN product_categories pc ON p.id=pc.product 
+        WHERE LOWER(p.name) LIKE LOWER('%$search%');";
+    } else {
+      $products_query = "SELECT * FROM products;";
+    }
+  }
+} else {
+  $products_query = "SELECT * FROM products;";
+}
+
+$products_result = mysqli_query($conn, $products_query);
+$products_count = mysqli_num_rows($products_result);
+$products = array();
+while ($row = mysqli_fetch_assoc($products_result)) {
+  $products[] = $row;
+}
 
 include "db_disconnect.php";
 
@@ -33,17 +108,72 @@ include "db_disconnect.php";
   ?>
 
   <div class="container">
+    <div class="search-wrapper">
+      <form action="products.php" method="GET" id="search-form">
+
+        <input id="search-input" type="text" placeholder="Search" name="search" />
+
+        <div class="category-checkboxes">
+          <?php
+
+          if (count($categories) > 0) {
+            foreach ($categories as $category) {
+              $category_name = $category["name"];
+              $category_id = $category["id"];
+              $category_count = $category["COUNT(*)"];
+              $category_checked = "";
+
+              if (in_array($category_id, $categories_checked)) {
+                $category_checked = "checked";
+              }
+
+              echo <<<END
+              <div class="category-checkbox">
+                <input type="checkbox" name="c[]" value="$category_id" id="$category_id" $category_checked/>
+                <label for="$category_id">$category_name ($category_count)</label>
+              </div>
+              END;
+            }
+          } else {
+            echo <<<END
+            <div class="no-categories">
+              <span>No categories!</span>
+            </div>
+            END;
+          }
+
+          ?>
+        </div>
+
+        <input type="submit" value="Search!" />
+      </form>
+    </div>
+
+    <div class="products-search-count">
+
+      <?php
+
+      if (isset($_GET["search"])) {
+        echo <<<END
+        <span>Search results: $products_count product(s)</span>
+        END;
+      }
+
+      ?>
+    </div>
+
+
     <div class="products">
 
       <?php
 
-      if (mysqli_num_rows($result) > 0) {
-        while ($product = mysqli_fetch_assoc($result)) {
+      if ($products_count > 0) {
+        foreach ($products as $product) {
           $product_id = $product["id"];
           $product_img = $product["image_url"];
           $product_name = $product["name"];
           $product_price = number_format($product["price"], 2);
-          echo "
+          echo <<<END
           <div class='product-list-item'>
             <div class='product-img-wrapper'>
               <a href='product.php?id=$product_id'>
@@ -62,11 +192,10 @@ include "db_disconnect.php";
               </div>
             </div>
           </div>
-          
-          ";
+          END;
         }
       } else {
-        echo "No products!";
+        echo "No products exist (yet)!";
       }
 
       ?>
@@ -83,8 +212,12 @@ include "db_disconnect.php";
             <span class='product-price'>&euro; $product_price</span>
             <div class='product-buttons'>
               <button class='product-buy'>BUY!</button>
-              <button class='product-cart'><ion-icon name="cart-outline"></ion-icon></button>
-              <button class='product-wishlist'><ion-icon name="heart-outline"></ion-icon></button>
+              <button class='product-cart'>
+                <ion-icon name="cart-outline"></ion-icon>
+              </button>
+              <button class='product-wishlist'>
+                <ion-icon name="heart-outline"></ion-icon>
+              </button>
             </div>
           </div>
         </div>
@@ -98,7 +231,7 @@ include "db_disconnect.php";
   ?>
   <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
   <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
-  
+
 
 </body>
 
