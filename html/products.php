4881a67796444128117fe072da26f3b7bb261c6d
jaspervalk <81962612+jaspervalk@users.noreply.github.com>
2022-01-31 15:16:01 +0100

    push

--- html/products.php
+++ html/products.php
@@ -97,7 +97,7 @@ include "db_disconnect.php";
   <link rel="stylesheet" href="css/products.css" />
   <script src="products.js" type="text/javascript"></script>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 
 <body>
