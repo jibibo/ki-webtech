555e51aefc895236ebfc273afcf1a3a285e299ae
Julian <julianibibo@gmail.com>
2022-01-31 15:00:55 +0100

    wtfffffffffff

--- html/products.php
+++ html/products.php
@@ -6,11 +6,7 @@ include "utils.php";
 // src: https://stackoverflow.com/a/6324360/13216113
 $result_categories = mysqli_query(
   $conn,
-  "SELECT name, id, COUNT(*)
-  FROM categories c 
-    JOIN product_categories pc ON c.id=pc.category 
-  GROUP BY c.name 
-  ORDER BY COUNT(*) DESC, c.name;"
+  "SELECT name, id, COUNT(*) FROM categories c JOIN product_categories pc ON c.id=pc.category GROUP BY c.name ORDER BY COUNT(*) DESC, c.name;"
 );
 $categories = array();
 while ($row = mysqli_fetch_assoc($result_categories)) {
