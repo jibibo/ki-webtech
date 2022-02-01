6ad0890081af0075426fa52bfd7db8b3f37abe3a
Julian <julianibibo@gmail.com>
2022-02-01 21:09:15 +0100

    users

--- html/user_session.php
+++ html/user_session.php
@@ -1,12 +1,15 @@
 <?php
 
+include "utils.php";
 $user_session = null;
 
 if (isset($_COOKIE["session_token"])) {
+
   include "db_connect.php";
 
   $session_token = htmlspecialchars($_COOKIE["session_token"]);
-  
+  pre_print("sestok $session_token<br>");
+
   $query_result = mysqli_query(
     $conn,
     "SELECT * 
@@ -15,11 +18,12 @@ if (isset($_COOKIE["session_token"])) {
     LIMIT 1"
   );
 
-  if (mysqli_num_rows($query_result) > 0) {
+  if ($query_result) {
     $user_session = mysqli_fetch_assoc($query_result);
+    pre_print($user_session);
+  } else {
+    pre_print("invalid sestok");
   }
 
   include "db_disconnect.php";
 }
-
-?>
