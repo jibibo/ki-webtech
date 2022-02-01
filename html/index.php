037be63ce8ade78ceec9b8e87b92e199761fb22a
Julian <julianibibo@gmail.com>
2022-02-01 22:05:22 +0100

    ico

--- html/index.php
+++ html/index.php
@@ -12,7 +12,7 @@ include "user_session.php";
   <meta charset="utf-8" />
   <meta http-equiv="x-ua-compatible" content="ie=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <link rel="icon" type="image/x-icon" href="/images/favicon.ico" />
+  <link rel="icon" type="image/x-icon" href="images/favicon.ico" />
   <link rel="stylesheet" href="css/global.css" />
   <link rel="stylesheet" href="css/navbar.css" />
   <link rel="stylesheet" href="css/footer.css" />
