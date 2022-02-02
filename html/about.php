3d9987871b130081b776c458eaf232ca7ac32b1d
Xinyu <xinyu.zhu2@student.uva.nl>
2022-02-02 01:16:39 +0100

    about us page

--- html/about.php
+++ html/about.php
@@ -13,14 +13,14 @@
   <link rel="stylesheet" href="css/about.css" />
 </head>
 
-<body>
+<body onload="document.body.style.opacity='1'">
   <?php
   include "navbar.php";
   ?>
  
-  <div class="container">
+  <!--<div class="container">-->
     <!-- <img class="nicepix" src="images/Location-Roeterseiland.jpeg" alt="UVA"> -->
-    <div class="about-us-text">
+    <!--<div class="about-us-text">
       <h1>About us</h1>
       <h5>How it started</h5>
       <p>UvAzon (not related to Amazon.com, Inc. in any form), is a small
@@ -28,6 +28,44 @@
         students (FOTO'S!) their aim is to provide any other student with all
         the supplies they need during their stay on the University of Amsterdan
         and Science Park.</p>
+    </div>
+  </div>-->
+  <div>
+    <div class="mission">
+        <img src="https://assets-cdn-folia.boondrive.com/1240x698/13924_1561558700220_UvA-SciencePark_Dag01_0605%20copy.jpg" alt="UvA Science park">
+    </div>
+    <div class="text">
+        <h1 class="header_title">OUR STORY</h1>
+        <p> We founded Uvazon at the heart of innovation and science, namely at Science Park Amsterdam.</p>
+        <p> Our main goal was to provide services for students to make their lives more flexible and easy.</p>
+        <p> Since we are students ourselves, we could understand a student's needs as no other.</p>
+        <p> Therefore we could no longer standby and we took immediately matters into our own hands.</p>
+        <p> After months of hard work and sleepless nights, we now present to you the solution of our future: Uvazon.</p>
+    </div>
+    <div class="mission">
+        <!--<div class="mission"><img src="https://student.uva.nl/binaries/_ht_1548843571179/pagebanner/content/gallery/faculteiten-en-diensten/restyling18-generiek-beeld/uva-summer-school-cycling-city-22072015_659%E2%94%AC%E2%8C%90-maartje-strijbis.jpg" alt="Students"></div>
+        <div class="mission"><img src="https://www.uva.nl/binaries/_ht_1631012828576/pagebanner/content/gallery/evenementen/2021/banner-uva-bachelorweek-v2.png" alt="students"></div> -->
+        <img src="https://summerschool.uva.nl/binaries/_ht_1614873822651/pagebanner/content/gallery/subsites/uva-summer-school/gsss-spo-wouter-van-der-wold-2019-4.jpg" alt="students">
+    </div>
+    <div class="text">
+        <h1 class="header_title">OUR MISSION</h1>
+        <p> We want to bring joy and inspiration to every student in the world.</p>
+        <p> In other words: to every human being in the world, because everyone is a student of life.</p>
+        <p> With everything that Uvazon has to offer, we hope to put a smile on people's face.</p>
+        <p> With our innovative services and our educational character, we hope to bring more color into the world.</p>
+        <p> We hope to be a place where we can always be there for one another.</p>
+    </div>
+    <div class="mission">
+        <img src="https://kwakmangroep.nl/site/assets/files/1426/uva-roeterseiland-2.jpg" alt="UvA university">
+    </div>
+    <div class="text">
+        <h1 class="header_title">OUR FUTURE</h1>
+        <p> In the future we hope to create a more accessible and dynamic platform.</p>
+        <p> Where our services could be accessed worldwide.</p>
+        <p> Where we can create an outstanding user experience.</p>
+        <p> Where we can grow together as a community.</p>
+        <p> And where we can bring Uvazon closer to you.</p>
+        </div>
     </div>
   </div>
 
