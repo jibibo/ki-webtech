555f3b660a8ec88d4187f77b32405b803a6d6ffd
jaspervalk <81962612+jaspervalk@users.noreply.github.com>
2022-02-02 15:18:04 +0100

    push

--- html/terms.php
+++ html/terms.php
@@ -12,7 +12,7 @@
   <link rel="stylesheet" href="css/global.css" />
   <link rel="stylesheet" href="css/navbar.css" />
   <link rel="stylesheet" href="css/footer.css" />
-  
+  <link rel="stylesheet" href="css/terms.css" />
 </head>
 
 <body>
