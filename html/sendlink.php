cdd58d0228a0e90ffdd258a225e2ce94fc697483
Xinyu <xinyu.zhu2@student.uva.nl>
2022-02-02 17:44:16 +0100

    test

--- html/sendlink.php
+++ html/sendlink.php
@@ -35,7 +35,7 @@ if(isset($_POST['reset_password']) && $_POST['input_email'])
     if($row) {
         $token = bin2hex(random_bytes(32));
         $id = $customer["id"];
-        mysqli_query($conn, "INSERT INTO reset_password_tokens (token, customer) VALUES ('$token', '$id')");
+        mysqli_query($conn, "INSERT INTO reset_password_tokens (token, customer) VALUES ('$token', $id)");
 
         // $update = mysqli_query($conn,"UPDATE users set password='" . $password . "', token='" . $token . "' WHERE email='" . $resetmail . "'");
         $link = "Click on the following link to reset your password: https://webtech-ki15.webtech-uva.nl/reset_password.php?key=".$resetmail."&token=".$token."";
