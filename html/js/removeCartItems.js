d25592ba7ea98cdf6fc2687f4ca3ca647881162a
Julian <julianibibo@gmail.com>
2022-02-03 14:41:32 +0100

    removecartitems

--- html/js/removeCartItems.js
+++ html/js/removeCartItems.js
@@ -1,25 +1,4 @@
 function removeCartItems() {
-  let oldCookie = getCookie();
+  document.cookie = "cart=";
   console.debug("updated", getCookie());
-}
-
-// from: https://www.w3schools.com/js/js_cookies.asp
-function getCookie() {
-  let name = "cart=";
-  let decodedCookie = decodeURIComponent(document.cookie);
-  let splitCookie = decodedCookie.split(";");
-  for (let i = 0; i < splitCookie.length; i++) {
-    let singleCookie = splitCookie[i];
-    while (singleCookie.charAt(0) == " ") {
-      singleCookie = singleCookie.substring(1);
-    }
-    if (singleCookie.indexOf(name) == 0) {
-      return singleCookie.substring(name.length);
-    }
-  }
-
-  return "";
-}
-
-  let newCookie = splitCookie.
 }
