e0978aca4d74f2b9fd2ea19d510f68bfb7e308f2
Julian <julianibibo@gmail.com>
2022-02-03 17:11:35 +0100

    order.php

--- html/order.php
+++ html/order.php
@@ -13,113 +13,103 @@ if (
   exit;
 }
 
-// get order info
-
-/*
-$first_name = htmlspecialchars($_POST["first_name"]);
-$last_name = htmlspecialchars($_POST["last_name"]);
-$address = htmlspecialchars($_POST["address"]);
-$zip = htmlspecialchars($_POST["zip"]);
-$country = htmlspecialchars($_POST["country"]);
-$email = htmlspecialchars($_POST["email"]);
-$phonenumber = htmlspecialchars($_POST["phonenumber"]);
-*/
-
 // input validation and order info
 
-function clean_data($data) {
-    $data = trim($data);
-    $data = stripslashes($data);
-    $data = htmlspecialchars($data);
-    return $data;
-} 
+$first_name = htmlspecialchars($_POST["first_name"]);
 
-$first_name = clean_data($_POST["first_name"]);
+// check if first name only consists of whitespaces and letters
+if (!preg_match("/^[a-zA-Z ]+$/", $first_name)) {
+  echo <<<END
+  <script language="JavaScript">
+  window.alert("Please enter a valid name");
+  window.location.href="https://webtech-ki15.webtech-uva.nl/checkout.php";
+  </script>
+  END;
+  exit;
+}
 
-  // check if first name only consists of whitespaces and letters
-  if (!preg_match("/^[a-zA-Z ]+$/", $first_name)) {
-      echo ("<script LANGUAGE='JavaScript'>
-      window.alert('Please enter a valid name');
-      window.location.href='https://webtech-ki15.webtech-uva.nl/checkout.php';
-      </script>");
-      exit;
-  }
+$last_name = htmlspecialchars($_POST["last_name"]);
 
-  $last_name = clean_data($_POST["last_name"]);
+// check if last name only consists of whitespaces and letters
+if (!preg_match("/^[a-zA-Z ]+$/", $last_name)) {
+  echo <<<END
+  <script language="JavaScript">
+  window.alert("Please enter a valid name");
+  window.location.href="https://webtech-ki15.webtech-uva.nl/checkout.php";
+  </script>
+  END;
+  exit;
+}
 
-  // check if last name only consists of whitespaces and letters
-  if (!preg_match("/^[a-zA-Z ]+$/", $last_name)) {
-      echo ("<script LANGUAGE='JavaScript'>
-      window.alert('Please enter a valid name');
-      window.location.href='https://webtech-ki15.webtech-uva.nl/checkout.php';
-      </script>");
-      exit;
-  }
+$address = htmlspecialchars($_POST["address"]);
 
-	$address = clean_data($_POST["address"]);
+// check if address is a valid address in the Netherlands
+if (!preg_match("/^([1-9][e][\s])*([a-zA-Z]+(([\.][\s])|([\s]))?)+[1-9][0-9]*(([-][1-9][0-9]*)|([\s]?[a-zA-Z]+))?$/i", $address)) {
+  echo <<<END
+  <script language="JavaScript">
+  window.alert("Please enter a valid address");
+  window.location.href="https://webtech-ki15.webtech-uva.nl/checkout.php";
+  </script>
+  END;
+  exit;
+}
 
-    // check if address is a valid address in the Netherlands
-    if (!preg_match("/^([1-9][e][\s])*([a-zA-Z]+(([\.][\s])|([\s]))?)+[1-9][0-9]*(([-][1-9][0-9]*)|([\s]?[a-zA-Z]+))?$/i", $address)) {
-        echo ("<script LANGUAGE='JavaScript'>
-        window.alert('Please enter a valid address');
-        window.location.href='https://webtech-ki15.webtech-uva.nl/checkout.php';
-        </script>");
-        exit;
-    }
+$zip = htmlspecialchars($_POST["zip"]);
 
-    $zip = clean_data($_POST["zip"]);
+// check if zipcode is a valid zipcode in the Netherlands
+if (!preg_match("/^[1-9][0-9]{3} ?(?!sa|sd|ss)[a-z]{2}$/i", $zip)) {
+  echo <<<END
+  <script language="JavaScript">
+  window.alert("Please enter a valid zipcode");
+  window.location.href="https://webtech-ki15.webtech-uva.nl/checkout.php";
+  </script>
+  END;
+  exit;
+}
 
-    // check if zipcode is a valid zipcode in the Netherlands
-    if (!preg_match("/^[1-9][0-9]{3} ?(?!sa|sd|ss)[a-z]{2}$/i", $zip)) {
-        echo ("<script LANGUAGE='JavaScript'>
-        window.alert('Please enter a valid zipcode');
-        window.location.href='https://webtech-ki15.webtech-uva.nl/checkout.php';
-        </script>");
-        exit;
-    }   
-    
-     $country = clean_data($_POST["country"]);
+$country = htmlspecialchars($_POST["country"]);
 
-    // check if country only consists of whitespaces and letters
-    if (!preg_match("/^[a-zA-Z ]+$/", $country)) {
-        $country_err = "Please enter a valid country";
-        echo ("<script LANGUAGE='JavaScript'>
-        window.alert('Please enter a valid country');
-        window.location.href='https://webtech-ki15.webtech-uva.nl/checkout.php';
-        </script>");
-        exit;
-    }
+// check if country only consists of whitespaces and letters
+if (!preg_match("/^[a-zA-Z ]+$/", $country)) {
+  echo <<<END
+  <script language="JavaScript">
+  window.alert("Please enter a valid country");
+  window.location.href="https://webtech-ki15.webtech-uva.nl/checkout.php";
+  </script>
+  END;
+  exit;
+}
 
-    $email = clean_data($_POST["email"]);
+$email = htmlspecialchars($_POST["email"]);
 
-    // check if email address is valid and well-formed
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        echo ("<script LANGUAGE='JavaScript'>
-        window.alert('Please enter a valid email address');
-        window.location.href='https://webtech-ki15.webtech-uva.nl/checkout.php';
-        </script>");
-        exit;
-    }
+// check if email address is valid and well-formed
+if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+  echo <<<END
+  <script language="JavaScript">
+  window.alert("Please enter a valid email address");
+  window.location.href="https://webtech-ki15.webtech-uva.nl/checkout.php";
+  </script>
+  END;
+  exit;
+}
 
-    $phonenumber = clean_data($_POST["phonenumber"]);
+$phonenumber = htmlspecialchars($_POST["phonenumber"]);
 
-    // check if phone number is a valid number in the Netherlands
-    // src regex: https://stackoverflow.com/a/123666/13216113
-    // https://regexr.com/3aevr
-    if (!preg_match("/^((\+|00(\s|\s?\-\s?)?)31(\s|\s?\-\s?)?(\(0\)[\-\s]?)?|0)[1-9]((\s|\s?\-\s?)?[0-9])((\s|\s?-\s?)?[0-9])((\s|\s?-\s?)?[0-9])\s?[0-9]\s?[0-9]\s?[0-9]\s?[0-9]\s?[0-9]$/", $phonenumber)) {
-        echo ("<script LANGUAGE='JavaScript'>
-        window.alert('Please enter a valid phone number');
-        window.location.href='https://webtech-ki15.webtech-uva.nl/checkout.php';
-        </script>");
-        exit;
-    }
-    // /^((\+|00(\s|\s?\-\s?)?)31(\s|\s?\-\s?)?(\(0\)[\-\s]?)?|0)[1-9]((\s|\s?\-\s?)?[0-9])((\s|\s?-\s?)?[0-9])((\s|\s?-\s?)?[0-9])\s?[0-9]\s?[0-9]\s?[0-9]\s?[0-9]\s?[0-9]$/gm
-
+// check if phone number is a valid number in the Netherlands
+// https://regexr.com/3aevr
+if (!preg_match("/^((\+|00(\s|\s?\-\s?)?)31(\s|\s?\-\s?)?(\(0\)[\-\s]?)?|0)[1-9]((\s|\s?\-\s?)?[0-9])((\s|\s?-\s?)?[0-9])((\s|\s?-\s?)?[0-9])\s?[0-9]\s?[0-9]\s?[0-9]\s?[0-9]\s?[0-9]$/", $phonenumber)) {
+  echo <<<END
+  <script language="JavaScript">
+  window.alert("Please enter a valid phone number");
+  window.location.href="https://webtech-ki15.webtech-uva.nl/checkout.php";
+  </script>
+  END;
+  exit;
+}
 
 // create order in db
 mysqli_query(
   $conn,
-  // "INSERT INTO `orders` (`id`, `first_name`, `last_name`, `address`, `zip`, `country`, `email`, `phone`, `total_price`, `date`, `status`) VALUES (NULL, 'sadf', 'asdf', 'sdf', 'sdf', 'sdf', 'sdf', 'sdf', '-1', CURRENT_TIMESTAMP, 'PROCESSING');  "
   "INSERT INTO orders (first_name, last_name, address, zip, country, email, phonenumber) 
   VALUES ('$first_name', '$last_name', '$address', '$zip', '$country', '$email', '$phonenumber')"
 );
@@ -182,6 +172,7 @@ mysqli_query(
   $conn,
   "UPDATE orders SET total_price=$subtotal WHERE id=$order_id;"
 );
+
 // clear user's cart cookie
 setcookie("cart");
 
@@ -191,6 +182,7 @@ $order_products_info_joined = join("\n", $order_products_info);
 ?>
 
 <html>
+
 <head>
   <title>Home | UvAzon</title>
   <meta charset="utf-8" />
@@ -211,11 +203,10 @@ $order_products_info_joined = join("\n", $order_products_info);
 
   <div class="container">
     <div class="message">
-      <?php 
-      echo 
-      "
-      <h1>Thank you for your purchase, ".$_POST['first_name']."! </h1>
-      ";
+      <?php
+      echo <<<END
+      <h1>Thank you for your purchase, $first_name! </h1>
+      END;
       ?>
     </div>
     <div class="order-info">
@@ -226,23 +217,22 @@ $order_products_info_joined = join("\n", $order_products_info);
           <td>Image</td>
         </tr>
         <br />
-        <?php //echo join("<br /><tr><td>", $order_products_info) ?>
-        
+        <?php //echo join("<br /><tr><td>", $order_products_info) 
+        ?>
+
         <?php
-        // include "utils.php";
-        // pre_print($product_counts);
         foreach ($product_ids_unique as $product_id) {
           // get product info
           $product_result = mysqli_query(
             $conn,
             "SELECT * FROM products WHERE id=$product_id"
           );
-        
+
           if (!$product_result) {
             // product id not found, ignore
             continue;
           }
-        
+
           $product = mysqli_fetch_assoc($product_result);
           $product_id = $product["id"];
           $product_name = $product["name"];
@@ -250,34 +240,34 @@ $order_products_info_joined = join("\n", $order_products_info);
           $subtotal += $product["price"] * $quantity;
           $image = $product["image_url"];
 
-          echo 
-          "
+          echo <<<END
           <tr>
             <td>$product_name</td>
             <td>$quantity</td>
-            <td class='right'> <img src='$image' width='200px' height='200px'></td>
+            <td class="right"> <img src="$image" width="200px" height="200px"></td>
           </tr>
-          
-          "; }
-          echo 
-          "
-          <tr class='subtotal'>
-            <td>Total price: €$subtotal</td>
-          </tr>
-          ";
+          END;
+        }
+        echo <<<END
+        <tr class="subtotal">
+          <td>Total price: €$subtotal</td>
+        </tr>
+        END;
         ?>
-          </td>
+
         </tr>
       </table>
     </div>
     <div class="return">
-          <form method="post">
-            <input type="submit" value="Return to home page" /> 
-          </form>
+      <form method="post">
+        <input type="submit" value="Return to home page" />
+      </form>
     </div>
   </div>
+
   <?php
   include "footer.php";
   ?>
 </body>
+
 </html>
