b99fd7d371f1c4a04d5e046861ea9bbb0cd63fe0
Julian <julianibibo@gmail.com>
2022-02-04 12:18:22 +0100

    sdf

--- html/footer.php
+++ html/footer.php
@@ -35,7 +35,7 @@
   </div>
 </div>
 
-<script src="js/activeNavLink.js"></script>
 <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
 <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="js/activeNavLink.js"></script>
